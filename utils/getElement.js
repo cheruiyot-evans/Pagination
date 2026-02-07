2aa3495c04c89de1b0f2d067b6bc2d34d81227c3
Cheruiyot Evans <chersoftwaredev@gmail.com>
2026-02-07 17:41:56 +0300

    Pagination updated

---
+++ utils/getElement.js
@@ -0,0 +1,7 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(`There was an error the '${selection}' does not exist`);
+}
