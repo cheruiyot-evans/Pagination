bdc0f62a11958e74de8e3c631e8c3d48786f0092
Cheruiyot Evans <chersoftwaredev@gmail.com>
2026-02-09 11:07:35 +0300

    buttons, next, prev working

--- app.js
+++ app.js
@@ -5,12 +5,44 @@ import displayButtons from './utils/displayButtons.js';
 import getElement from './utils/getElement.js';
 
 const title = getElement('.section-title>h1');
+const btnContainer = getElement('.btn-container');
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+};
 
 const innit = async () => {
   const followers = await fetchFollowers();
-  displayFollowers(paginate(followers)[0]);
   title.textContent = 'pagination';
-  const pages = paginate(followers);
+  pages = paginate(followers);
+  setupUI();
 };
+
+btnContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('btn-container')) return;
+  if (e.target.classList.contains('page-btn')) {
+    const currentIndex = parseInt(e.target.dataset.index);
+    index = currentIndex;
+  }
+  if (e.target.classList.contains('next-btn')) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+
+    console.log(pages.length);
+  }
+  if (e.target.classList.contains('prev-btn')) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  setupUI();
+});
 
 window.addEventListener('load', innit);
